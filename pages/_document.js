2a02d55fcd921341b8bf87c012453ab352d7a376
Charles ⚡ <charlespeters42@gmail.com>
2018-03-04 22:10:52 -0800

    adding SEO crap

--- pages/_document.js
+++ pages/_document.js
@@ -17,8 +17,12 @@ export default class MyDocument extends Document {
         <Head>
           <title>Walt Peters | Gardening Consultant</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta
+            name="google-site-verification"
+            content="h6NhLEISPBdUzO9kTpiy-Y6MIKDCJCclBOSIzIo50vY"
+          />
           <meta name="theme-color" content="#d8a75b" />
-
           <meta
             name="description"
             content="An award-winning landscaping expert with over 50 years of experience, looking to equip and teach the community how to grow, garden and invest in back in their own communities in Central PA."
