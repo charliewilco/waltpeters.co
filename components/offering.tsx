66dec3c4b464046503651884897e4e9e97e9ee0b
Charlie ⚡ <charliewilc0@protonmail.com>
2022-03-27 01:49:32 -0700

    reducing overhead

--- components/offering.tsx
+++ components/offering.tsx
@@ -1,34 +1,25 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
-interface OfferingProps {
+interface IOfferingProps {
   title: string;
-  content: string;
-  icon: string;
+  icon: StaticImageData;
 }
 
-export const OfferingBlock: React.VFC<OfferingProps> = ({
+export const OfferingBlock: React.FC<IOfferingProps> = ({
   title,
-  content,
   icon,
+  children,
 }) => (
-  <div className="OfferingContainer">
-    <div className="OfferingIconContainer">
+  <div className="off-container">
+    <div className="off-icon-container">
       {icon && (
-        <Image
-          alt=""
-          className="OfferingIcon"
-          width={40}
-          height={40}
-          objectFit="contain"
-          layout="responsive"
-          src={icon}
-        />
+        <Image alt="" width={40} height={40} objectFit="contain" src={icon} />
       )}
     </div>
-    <h3 className="OfferingTitle">{title}</h3>
-    <p className="OfferingContent">{content}</p>
+    <h4>{title}</h4>
+    <p>{children}</p>
     <style jsx>{`
-      .OfferingContainer {
+      .off-container {
         margin-bottom: 1rem;
         background: #fefdfb;
         box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.23),
@@ -36,29 +27,32 @@ export const OfferingBlock: React.VFC<OfferingProps> = ({
         padding: 1rem;
       }
 
-      .OfferingIcon {
+      .off-icon {
         display: block;
       }
 
-      .OfferingTitle {
+      h4 {
         font-size: 1.25rem;
         font-weight: 500;
         margin-bottom: 0.875rem;
       }
 
-      .OfferingContent {
+      p {
         font-size: 0.875rem;
         line-height: 1.7;
         text-align: left;
+        margin: 0;
       }
 
-      .OfferingIconContainer {
+      .off-icon-container {
         background-color: #d8a75b;
         border-radius: 50%;
         width: 4rem;
         height: 4rem;
         padding: 0.75rem 0.25rem;
         margin-bottom: 1.5rem;
+        display: flex;
+        justify-content: center;
       }
     `}</style>
   </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
-import { NextPage } from 'next';
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
 import Head from 'next/head';
+import { FiMail, FiPhoneCall } from 'react-icons/fi';
+
 import { OfferingBlock } from '../components/offering';
 import { HeroBanner } from '../components/banner';
-import { Contact } from '../components/contact';
-import { SkillSharing } from '../components/skillsharing';
-import data from '../content.json';
 
-const IndexPage: NextPage = () => (
+import planterIcon from '../public/static/planter.svg';
+import handgrowthIcon from '../public/static/handgrowth.svg';
+import flowerIcon from '../public/static/flower.svg';
+
+export const getStaticProps: GetStaticProps<{ year: string }> = async () => {
+  return {
+    props: {
+      year: new Date().getFullYear().toString(),
+    },
+    revalidate: false,
+  };
+};
+
+const IndexPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  year,
+}) => (
   <div>
     <Head>
       <title>Walt Peters | Gardening Consultant</title>
@@ -24,28 +38,76 @@ const IndexPage: NextPage = () => (
     </Head>
     <main>
       <section>
-        <HeroBanner {...data} />
+        <HeroBanner name="Walt Peters" title="Gardening Consultant">
+          An award-winning landscaping expert with over 50 years of experience,
+          looking to equip and teach the community how to grow, garden and
+          invest in back in their own communities in Central PA.
+        </HeroBanner>
       </section>
       <section>
         <h3>Offerings</h3>
         <div className="offerings">
-          {data.offerings.map((offer, i) => (
-            <OfferingBlock key={i} {...offer} />
-          ))}
+          <OfferingBlock title="On-Site Visits" icon={planterIcon}>
+            Need garden advice or have a difficult garden problem? I will come
+            to your garden site, take a thorough look, and personally consult
+            with you to grow the best garden possible.
+          </OfferingBlock>
+          <OfferingBlock title="1:1 Training" icon={handgrowthIcon}>
+            For DIYers or small businesses, I offer one-on-one training for
+            efficient gardening and landscaping practices. I offer individual
+            training and host regular workshops to help grow your knowledge and
+            skill.
+          </OfferingBlock>
+          <OfferingBlock title="Consultation" icon={flowerIcon}>
+            Need help making the most out of your yard space? Don’t know which
+            plants or trees will grow best in your yard? I have 50 years of
+            experience designing beautiful landscapes appropriate to your space
+            and environment.
+          </OfferingBlock>
         </div>
       </section>
-      <section>
-        <SkillSharing />
-      </section>
-      <section>
+      <article>
+        <h3>Skill-sharing for the Community</h3>
+        <p>
+          <b>Skill-sharing</b> simply put, is sharing expert skills with another
+          party interested in acquiring them. With over 50 years of experience
+          and award-winning work, I hope to be able to share my skills with each
+          of you. Central Pennsylvania is home to tremendous valleys, mountain
+          ranges and fields, and there is so much history and potential in
+          cultivating a healthy garden in this region. I’ll be offering group
+          seminars in the coming weeks and currently offer one-on-one training.
+        </p>
+        <p>
+          <b>Get in Contact Today!</b>
+        </p>
+      </article>
+      <aside>
         <h3>Contact</h3>
-        <Contact />
-      </section>
+        <address>
+          <div>
+            <FiPhoneCall color="#d8a75b" size={24} />
+            <h4>Call</h4>
+            <p>
+              Available by phone or text during normal business hours.
+              717-543-7475
+            </p>
+            <a href="tel:+17175437475">Call Me</a>
+          </div>
+
+          <div>
+            <FiMail color="#d8a75b" size={24} />
+            <h4>Email</h4>
+            <p>
+              Email is the best way to get a conversation started about your
+              individual needs.
+            </p>
+            <a href="mailto:waltpeters42@gmail.com">Email Me</a>
+          </div>
+        </address>
+      </aside>
     </main>
-    <footer className="PageFooter">
-      <div>
-        <span>&copy; Copyright {new Date().getFullYear()} Walt Peters.</span>
-      </div>
+    <footer>
+      <span>&copy; Copyright {year} Walt Peters.</span>
     </footer>
     <style jsx>{`
       h3 {
@@ -65,9 +127,12 @@ const IndexPage: NextPage = () => (
       main {
         padding-top: 0.5rem;
       }
+
+      article,
+      aside,
       section,
       footer {
-        margin-bottom: 3rem;
+        margin-bottom: 4rem;
       }
 
       main,
@@ -78,9 +143,53 @@ const IndexPage: NextPage = () => (
         margin-left: auto;
         margin-right: auto;
       }
+
+      article {
+        max-width: 80ch;
+        margin-right: auto;
+      }
+
+      article p {
+        font-size: 112.5%;
+        line-height: 1.7;
+      }
+
+      b {
+        color: #d8a75b;
+      }
+
+      h4 {
+        font-size: 1.25rem;
+        margin-top: 1rem;
+        margin-bottom: 1.5rem;
+      }
+
+      a {
+        padding: 0.5rem;
+        display: inline-block;
+        background-color: #d8a75b;
+        color: #fefdfb;
+        text-decoration: none;
+        border-radius: 4px;
+      }
+
+      address p {
+        margin-bottom: 1rem;
+      }
+      address {
+        --columns: 1;
+        display: grid;
+        gap: 2rem;
+        margin: 0 auto;
+        grid-template-columns: repeat(var(--columns), minmax(0, 1fr));
+      }
+
       @media (min-width: 42rem) {
         main {
           padding-top: 1rem;
+        }
+        address {
+          --columns: 2;
         }
         .offerings {
           --columns: 3;
